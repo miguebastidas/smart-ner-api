b958df51e4a3c5aa283fc08b9321be5dff36e542
miguebastidas <miguebasti05@gmail.com>
2025-12-17 09:44:40 -0500

    init app completed using fastapi and uvicorn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from smart-ner-api!")
+from fastapi import FastAPI
+import uvicorn
 
+app = FastAPI()
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app, host="0.0.0.0", port=8000)
