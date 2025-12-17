e4d909f27091c123c72261a643b66e4fa674ae0b
miguebastidas <miguebasti05@gmail.com>
2025-12-17 09:48:14 -0500

    add fastapi lifespan

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 import uvicorn
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup code here
+    print("Starting up...")
+    yield
+    # Shutdown code here
+    print("Shutting down...")
+
+app = FastAPI(lifespan=lifespan)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
